71e7b939d176eb89c134d8b9c2a9765921462fdb
EduardoBrandt <eduardobrandt61@gmail.com>
2021-08-20 13:55:16 -0300

    Finalizado

--- model.py
+++ model.py
@@ -1,7 +1,17 @@
+from sqlalchemy.sql.elements import Null
 from config import *
 
 class PessoaFisica(db.Model):
     __tablename__ = 'pessoa_fisica'
+
+    def __init__(self, nome, telefone, cidade, analise_sobre, desejo, operadora, aceitar_termos):
+        self.nome = nome
+        self.telefone = telefone
+        self.cidade = cidade
+        self.analise_sobre = analise_sobre
+        self.desejo = desejo
+        self.operadora = operadora
+        self.aceitar_termos = aceitar_termos
 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
@@ -12,13 +22,26 @@ class PessoaFisica(db.Model):
     desejo = db.Column(db.String(500))
     operadora = db.Column(db.String(100))
 
-    fatura = db.Column(db.LargeBinary)
+    #fatura = db.Column(db.LargeBinary, nullable = True)
 
-    aceitar_termos = db.Column(db.String(100))
+    aceitar_termos = db.Column(db.String(100), default = 'Sim')
 
 
 class PessoaJuridica(db.Model):
     __tablename__ = 'pessoa_juridica'
+
+    def __init__(self, nome, nome_empresa, setor, email,telefone, cidade, analise_sobre, desejo, operadora, aceitar_termos):
+        self.nome = nome
+        self.nome_empresa = nome_empresa
+        self.setor = setor
+        self.email = email
+        self.telefone = telefone
+        self.cidade = cidade
+        self.analise_sobre = analise_sobre
+        self.desejo = desejo
+        self.operadora = operadora
+        self.aceitar_termos = aceitar_termos
+
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -33,16 +56,24 @@ class PessoaJuridica(db.Model):
     desejo = db.Column(db.String(500))
     operadora = db.Column(db.String(100))
 
-    aceitar_termos = db.Column(db.String(100))
+    #fatura = db.Column(db.LargeBinary, nullable = True)
+
+    aceitar_termos = db.Column(db.String(100), default = 'Sim')
 
 
 
 
 if __name__ == '__main__':
     if os.path.exists(arquivobd):
-        print('Arquivo já existente!')
+        os.remove(arquivobd)
     else:
         db.create_all()
+        x = PessoaJuridica('Jonas', 'Fey', 'Comercial', 'jonas@gmail.com', '11 99999-9999', 'São Paulo', 'Internet', 'Aumentar', 'Vivo', 'Sim')
+        y = PessoaFisica('Claudio', '11 99999-9999', 'Rio de Janeiro', 'Móvel', 'Aumentar dados', 'Claro', 'Sim')
+
+        db.session.add(x)
+        db.session.add(y)
+        db.session.commit()
 
 
 
--- routes.py
+++ routes.py
@@ -1,6 +1,11 @@
 from config import *
+from model import PessoaFisica, PessoaJuridica
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    clientes_fisico = PessoaFisica.query.all()
+    clientes_juridico = PessoaJuridica.query.all()
+
+    return render_template('index.html', clientes_fisico=clientes_fisico, 
+    clientes_juridico=clientes_juridico)
 
