ceec8f89e3c4484945d638424acfcabd220b9c6b
EduardoBrandt <eduardobrandt61@gmail.com>
2021-08-20 09:58:00 -0300

    Atualizando os dados do db

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
-
 import os
-
 
 caminho = os.path.dirname(os.path.abspath(__file__))    
 arquivobd = os.path.join(caminho, "storage.db") 
 
 app = Flask(__name__)
-
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"+arquivobd 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
--- model.py
+++ model.py
@@ -1,29 +1,49 @@
-from flask_sqlalchemy import Pagination
 from config import *
 
+class PessoaFisica(db.Model):
+    __tablename__ = 'pessoa_fisica'
 
-class Pessoa_Anexo(db.Model):
-    __tablename__ = "pessoa_anexo"
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
-    telefone = 
-    email =
-    termos de uso
-    fatura
+    telefone = db.Column(db.String(100))
+
+    cidade = db.Column(db.String(100))
+    analise_sobre = db.Column(db.String(100))
+    desejo = db.Column(db.String(500))
+    operadora = db.Column(db.String(100))
+
+    fatura = db.Column(db.LargeBinary)
+
+    aceitar_termos = db.Column(db.String(100))
 
 
-class Pessoa_Preencher(db.Model):
-    '''
-    PF/PJ
-    LOCALIDADE
-    INTERNET/MOVEL/Fixo 
-    operadora
-    VALOR QUE Paga  
-    pERGUNTAS QUE ELE TEM QUE SELECIONAR(AUMENTAR PLANO ETC...) QUE VAI SER DIFERENTE PARA CADA UM QUE ELE ESCOLHER DE (INTERNET/MOVEL/Fixo)
-    DISPONIBILIDADE
+class PessoaJuridica(db.Model):
+    __tablename__ = 'pessoa_juridica'
+
+    id = db.Column(db.Integer, primary_key=True)
+
     nome = db.Column(db.String(100))
-    telefone = 
-    email =
-    termos de uso
-    '''
+    nome_empresa = db.Column(db.String(100))
+    setor = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    telefone = db.Column(db.String(100))
 
+    cidade = db.Column(db.String(100))
+    analise_sobre = db.Column(db.String(100))
+    desejo = db.Column(db.String(500))
+    operadora = db.Column(db.String(100))
+
+    aceitar_termos = db.Column(db.String(100))
+
+
+
+
+if __name__ == '__main__':
+    if os.path.exists(arquivobd):
+        print('Arquivo já existente!')
+    else:
+        db.create_all()
+
+
+
+    
